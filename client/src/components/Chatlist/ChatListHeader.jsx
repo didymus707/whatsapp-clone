b5b8ab8c370019a0e07e60be7893c079cf7ab6f8
Adewale Orotayo <didymus7007@gmail.com>
2024-01-23 17:22:27 +0100

    fixed onboarding and login features

--- client/src/components/Chatlist/ChatListHeader.jsx
+++ client/src/components/Chatlist/ChatListHeader.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Avatar from "../common/Avatar";
+import { useStateProvider } from "@/context/StateContext";
+import { BsFillChatLeftTextFill, BsThreeDotsVertical } from "react-icons/bs";
 
 function ChatListHeader() {
-  return <div>ChatListHeader</div>;
+  const [{ userInfo }, dispatch] = useStateProvider();
+  console.log(userInfo);
+  return (
+    <div className="h-16 px-4 py-3 flex justify-between items-center">
+      <div className="cursor-pointer">
+        <Avatar type="sm" image={userInfo?.profileImage} />
+      </div>
+      <div className="flex gap-6">
+        <BsFillChatLeftTextFill
+          title="New Chat"
+          className="text-panel-header-icon cursor-pointer text-xl"
+        />
+        <>
+          <BsThreeDotsVertical
+            title="Menu"
+            className="text-panel-header-icon cursor-pointer text-xl"
+          />
+        </>
+      </div>
+    </div>
+  );
 }
 
 export default ChatListHeader;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,44 @@
-import React from "react";
-import ChatList from "./Chatlist/ChatList";
+import axios from "axios";
 import Empty from "./Empty";
+import { data } from "autoprefixer";
+import { useRouter } from "next/router";
+import ChatList from "./Chatlist/ChatList";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { reducerCases } from "@/context/constants";
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
+import { firebaseAuth } from "@/utils/FirebaseConfig";
+import { useStateProvider } from "@/context/StateContext";
 
 function Main() {
+  const router = useRouter();
+  const [{ userInfo }, dispatch] = useStateProvider();
+  const [redirectLogin, setRedirectLogin] = useState(false);
+
+  useEffect(() => {
+    if (redirectLogin) router.push("/login");
+  }, [redirectLogin]);
+
+  onAuthStateChanged(firebaseAuth, async (currentUser) => {
+    if (!currentUser) setRedirectLogin(true);
+    if (!userInfo && currentUser?.email) {
+      const {
+        data: { data },
+      } = await axios.post(CHECK_USER_ROUTE, {
+        email: currentUser.email,
+      });
+      if (!data.status) {
+        router.push("/login");
+      }
+      console.log({ data: data });
+      const { id, name, email, profilePicture: profileImage, status } = data;
+      dispatch({
+        type: reducerCases.SET_USER_INFO,
+        userInfo: { id, name, email, profileImage, status },
+      });
+    }
+  });
+
   return (
     <>
       <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
