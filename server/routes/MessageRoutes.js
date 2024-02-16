886a701a007e94a3eea7a7dab89bb6cb51cbc97e
Adewale Orotayo <didymus7007@gmail.com>
2024-02-17 00:41:08 +0100

    added the messages routes

---
+++ server/routes/MessageRoutes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { addMessage } from "../controllers/MessageController";
+
+const router = Router();
+router.post("/add-message", addMessage);
+
+export default router;
