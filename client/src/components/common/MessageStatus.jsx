475e6631d20604313b5dbd2406dee8b46605db7b
Adewale Orotayo <didymus7007@gmail.com>
2024-03-09 16:11:23 +0100

    setting message status based on sent, read or delivered

--- client/src/components/common/MessageStatus.jsx
+++ client/src/components/common/MessageStatus.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BsCheck, BsCheckAll } from "react-icons/bs";
 
 function MessageStatus() {
-  return <div>MessageStatus</div>;
+  return (
+    <>
+      {MessageStatus.messageStatus === "sent" && (
+        <BsCheck className="text-lg" />
+      )}
+      {MessageStatus.messageStatus === "delivered" && (
+        <BsCheckAll className="text-lg" />
+      )}
+      {MessageStatus.messageStatus === "read" && (
+        <BsCheckAll className="text-lg text-icon-ack" />
+      )}
+    </>
+  );
 }
 
 export default MessageStatus;
